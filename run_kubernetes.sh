9005594fe6ab7b3127c4db32f8ca251fc983d171
Deepak Gupta <dgupta@silver-peak.com>
2020-01-19 18:24:10 +0530

    kubes done

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,21 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="deepakmits/api"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run apilocal\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=apilocal
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward apilocal 8000:80
 
